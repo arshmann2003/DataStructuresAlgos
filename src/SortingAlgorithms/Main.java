351096c1962648547920bae2c24cb6d94244c05a
Arsh Mann <arshmann@Arshs-MacBook-Air-3.local>
2023-06-24 00:04:40 -0700

    selection sort

--- src/SortingAlgorithms/Main.java
+++ src/SortingAlgorithms/Main.java
@@ -3,7 +3,8 @@ package SortingAlgorithms;
 public class Main {
     public static void main(String[] args){
         int[] arr = {5,4,2,15,2,234,43,2,1};
-        MergeSort.mergeSort(arr);
+//        MergeSort.mergeSort(arr);
+        SelectionSort.selectionSort(arr);
 
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + " ");
