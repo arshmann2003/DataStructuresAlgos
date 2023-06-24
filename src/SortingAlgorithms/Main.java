b19b40cf64a1160b6131bed92bbfaac7ebd16ac3
Arsh Mann <arshmann@Arshs-MacBook-Air-3.local>
2023-06-23 23:56:43 -0700

    merge sort algorithm

---
+++ src/SortingAlgorithms/Main.java
@@ -0,0 +1,13 @@
+package SortingAlgorithms;
+
+public class Main {
+    public static void main(String[] args){
+        int[] arr = {5,4,2,15,2,234,43,2,1};
+        MergeSort.mergeSort(arr);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
---
+++ src/SortingAlgorithms/MergeSort.java
@@ -0,0 +1,42 @@
+package SortingAlgorithms;
+
+public class MergeSort {
+
+        public static void mergeSort(int[] array) {
+            if (array.length <= 1) {
+                return;
+            }
+            int[] temp = new int[array.length];
+            mergeSort(array, temp, 0, array.length - 1);
+        }
+
+        private static void mergeSort(int[] array, int[] temp, int left, int right) {
+            if (left >= right) {
+                return;
+            }
+            int mid = (left + right) / 2;
+            mergeSort(array, temp, left, mid);
+            mergeSort(array, temp, mid + 1, right);
+            merge(array, temp, left, mid, right);
+        }
+
+        private static void merge(int[] array, int[] temp, int left, int mid, int right) {
+            for (int i = left; i <= right; i++) {
+                temp[i] = array[i];
+            }
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+
+            while (i <= mid && j <= right) {
+                if (temp[i] <= temp[j]) {
+                    array[k++] = temp[i++];
+                } else {
+                    array[k++] = temp[j++];
+                }
+            }
+            while (i <= mid) {
+                array[k++] = temp[i++];
+            }
+        }
+}
